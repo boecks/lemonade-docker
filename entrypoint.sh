ba4100e80ffeb8220e0a9f0483473cc1176aafff
sascha <sascha.boeck@sbzone.at>
2026-03-09 17:52:44 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# Auto-unload im Hintergrund starten (nur wenn LEMONADE_KEEPALIVE > 0)
+# Auto-unload im Hintergrund starten (nur wenn LEMONADE_KEEPALIVE gesetzt und != 0)
 if [ -n "${LEMONADE_KEEPALIVE}" ] && [ "${LEMONADE_KEEPALIVE}" != "0" ]; then
     python3 /opt/auto_unload.py &
 fi
 
-exec lemonade-server serve
+# lemonade-server mit Timestamp-Prefix auf stdout
+exec lemonade-server serve 2>&1 | ts '[%Y-%m-%d %H:%M:%S]'
