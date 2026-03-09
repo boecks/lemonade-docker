d43ee699879e890eca23bf6c247d80e0b9660984
sascha <sascha.boeck@sbzone.at>
2026-03-09 17:40:43 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Auto-unload im Hintergrund starten (nur wenn LEMONADE_KEEPALIVE > 0)
-if [ "${LEMONADE_KEEPALIVE:-0}" -gt 0 ]; then
+if [ -n "${LEMONADE_KEEPALIVE}" ] && [ "${LEMONADE_KEEPALIVE}" != "0" ]; then
     python3 /opt/auto_unload.py &
 fi
 
